282d1c9015922dae475f0a907c6d1f1f2bf5b702
Tushar Patel <tusharpatel@Tushars-MacBook-Pro.local>
2024-10-04 18:06:43 -0400

    add About.js

---
+++ src/pages/About.js
@@ -0,0 +1,44 @@
+import Accordion from 'react-bootstrap/Accordion';
+
+function About() {
+    return (<main className='container'>
+        <div className='row'>
+            <div className='col-md-5'>
+
+                <h1>Tushar Patel</h1>
+                <img src="" alt="headshot" className="img-fluid" />
+            </div>
+            <div className='col-md-7'>
+                <Accordion>
+                    <Accordion.Item eventKey="0">
+                        <Accordion.Header>Accordion Item #1</Accordion.Header>
+                        <Accordion.Body>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                            culpa qui officia deserunt mollit anim id est laborum.
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="2">
+                        <Accordion.Header>Skills</Accordion.Header>
+                        <Accordion.Body>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                            culpa qui officia deserunt mollit anim id est laborum.
+                        </Accordion.Body>
+                    </Accordion.Item>
+                </Accordion>
+            </div>
+        </div>
+    </main>
+    );
+}
+
+export default About;
